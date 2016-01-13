bd7ea2ee3f242f4c1c040cbdf57f4837a9e19336
koenkoe <koenhabets1@gmail.com>
2016-01-13 21:28:13 +0100

    ...

--- app/src/main/java/com/koenhabets/school/MainActivity.java
+++ app/src/main/java/com/koenhabets/school/MainActivity.java
@@ -5,25 +5,79 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.android.volley.*;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView textView;
+    RequestQueue requestQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        requestQueue = Volley.newRequestQueue(this);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        textView = (TextView) findViewById(R.id.textView);
 
+        setSupportActionBar(toolbar);
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                textView.setText("");
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+                JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, "https://api.scholica.com/2.0/communities/1/calendar/schedule?token=bb299b44ed98293bd9fd4a4b97fcb536f387",
+                        new Response.Listener<JSONObject>() {
+                            @Override
+                            public void onResponse(JSONObject response) {
+                                try {
+                                    JSONObject jsonMain = response.getJSONObject("result");
+                                    JSONArray jsonArray = jsonMain.getJSONArray("items");
+                                    for(int i = 0 ; i <jsonArray.length();i++ ) {
+                                        Log.i("d", response + "d");
+
+                                        String d = jsonMain.getString("title");
+
+                                        JSONObject vak = jsonArray.getJSONObject(i);
+                                        String title = vak.getString("title");
+                                        String lokaal = vak.getString("subtitle");
+                                        textView.append(title + " " + lokaal + "\n");
+                                    }
+
+
+                                } catch (JSONException e) {
+                                        e.printStackTrace();
+                                }
+                            }
+                        },
+
+                        new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                Snackbar snackbar = Snackbar.make(findViewById(android.R.id.content), "Error: " + error.getMessage(), Snackbar.LENGTH_LONG);
+                                snackbar.show();
+                                //Log.e("error", error.getMessage());
+                            }
+                        }
+                );
+                requestQueue.add(jsonObjectRequest);
             }
         });
     }
@@ -49,4 +103,12 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+    private Response.ErrorListener errorListener = new Response.ErrorListener() {
+        @Override
+        public void onErrorResponse(VolleyError error) {
+            Snackbar snackbar = Snackbar.make(findViewById(android.R.id.content), "Error: " + error.getMessage(), Snackbar.LENGTH_LONG);
+            snackbar.show();
+            Log.e("error", error.getMessage());
+        }
+    };
 }
