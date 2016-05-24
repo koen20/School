aef9b01a95b5a9e480552058db4a842730c2d746
Koen Habets <koenhabets1@gmail.com>
2016-05-24 15:54:20 +0200

    Added details to grades

--- app/src/main/java/com/koenhabets/school/activities/GradesActivity.java
+++ app/src/main/java/com/koenhabets/school/activities/GradesActivity.java
@@ -3,12 +3,19 @@ package com.koenhabets.school.activities;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
 import android.widget.TextView;
 
 import com.koenhabets.school.R;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class GradesActivity extends AppCompatActivity {
     TextView textView;
+    TextView textView2;
+    String text = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,11 +23,33 @@ public class GradesActivity extends AppCompatActivity {
         setContentView(R.layout.activity_grades);
 
         textView = (TextView) findViewById(R.id.textView);
+        textView2 = (TextView) findViewById(R.id.textView2);
 
         Intent intent = getIntent();
         String subject = intent.getStringExtra("subject");
         String response = intent.getStringExtra("response");
 
-        textView.setText(subject);
+
+        try {
+            JSONObject jsonObject = new JSONObject(response);
+            JSONObject jsonMain = jsonObject.getJSONObject("grades");
+            JSONObject vak = jsonMain.getJSONObject(subject);
+            textView.setText(subject + ": " + vak.getDouble("avg"));
+            JSONArray grades = vak.getJSONArray("grades");
+            for (int i = 0; i < grades.length(); i++) {
+                JSONObject grade = grades.getJSONObject(i);
+                String title = grade.getString("title");
+                Double avg = grade.getDouble("grade");
+                if (avg > 5.9) {
+                    text += title + ": " + "<font color=green>" + avg + "</font><br>";
+                } else if (avg < 6) {
+                    text += title + ": " + "<font color=red>" + avg + "</font><br>";
+                }
+
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        textView2.setText(Html.fromHtml(text));
     }
 }
