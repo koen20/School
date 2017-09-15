308b66b5e44dbf2716a91de261dd9a9db5c14dc2
Koen Habets <koenhabets1@gmail.com>
2017-09-15 21:23:29 +0200

    Fixed login crash

--- app/src/main/java/com/koenhabets/school/activities/LoginActivity.java
+++ app/src/main/java/com/koenhabets/school/activities/LoginActivity.java
@@ -28,6 +28,8 @@ public class LoginActivity extends AppCompatActivity {
     EditText editTextUsername;
     EditText editTextPassword;
     RequestQueue requestQueue;
+    boolean zermeloLogin;
+    boolean somLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,19 +46,23 @@ public class LoginActivity extends AppCompatActivity {
         TokenRequest tokenRequest = new TokenRequest(editTextZermelo.getText().toString(), new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
+                zermeloLogin = true;
                 SharedPreferences sharedPref = SchoolApp.getContext().getSharedPreferences("com.koenhabets.school", Context.MODE_PRIVATE);
                 SharedPreferences.Editor editor = sharedPref.edit();
                 editor.putBoolean("Logged-in", true);
                 editor.putString("zermeloAccessToken", response);
                 editor.apply();
-                Intent intent = new Intent(SchoolApp.getContext(), DrawerActivity.class);
-                startActivity(intent);
+                if (somLogin && zermeloLogin) {
+                    Intent intent = new Intent(SchoolApp.getContext(), DrawerActivity.class);
+                    startActivity(intent);
+                }
             }
         }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
+                zermeloLogin = false;
                 Log.e("error", "" + error.getMessage());
-                AlertDialog alertDialog = new AlertDialog.Builder(getApplicationContext()).create();
+                AlertDialog alertDialog = new AlertDialog.Builder(LoginActivity.this).create();
                 alertDialog.setTitle(getString(R.string.login_failed));
                 alertDialog.setMessage(getString(R.string.incorrect_zermelo));
                 alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, getString(R.string.ok),
@@ -83,28 +89,32 @@ public class LoginActivity extends AppCompatActivity {
                     SharedPreferences sharedPref = SchoolApp.getContext().getSharedPreferences("com.koenhabets.school", Context.MODE_PRIVATE);
                     SharedPreferences.Editor editor = sharedPref.edit();
                     editor.putBoolean("Logged-in", true);
+                    Log.i("somaccess", jsonObject.getString("access_token"));
                     editor.putString("somAccessToken", jsonObject.getString("access_token"));
                     editor.putString("somRefreshToken", jsonObject.getString("refresh_token"));
                     editor.apply();
-
+                    somLogin = true;
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
-                Intent intent = new Intent(SchoolApp.getContext(), DrawerActivity.class);
-                startActivity(intent);
+                if (somLogin && zermeloLogin) {
+                    Intent intent = new Intent(SchoolApp.getContext(), DrawerActivity.class);
+                    startActivity(intent);
+                }
             }
         }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
+                somLogin = false;
                 Log.e("error", "" + error.getMessage());
                 SharedPreferences sharedPref = SchoolApp.getContext().getSharedPreferences("com.koenhabets.school", Context.MODE_PRIVATE);
                 SharedPreferences.Editor editor = sharedPref.edit();
                 editor.clear();
                 editor.apply();
-                AlertDialog alertDialog = new AlertDialog.Builder(getApplicationContext()).create();
+                AlertDialog alertDialog = new AlertDialog.Builder(LoginActivity.this).create();
                 alertDialog.setTitle(getString(R.string.login_failed));
                 alertDialog.setMessage(getString(R.string.incorrect_som));
-                alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "getString(R.string.ok)",
+                alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, getString(R.string.ok),
                         new DialogInterface.OnClickListener() {
                             public void onClick(DialogInterface dialog, int which) {
                                 dialog.dismiss();
